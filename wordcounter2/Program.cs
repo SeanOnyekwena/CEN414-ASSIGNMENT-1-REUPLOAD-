7e0c0b97726576eec226bdd10a28375085696b77
SeanOnyekwena <75506154+SeanOnyekwena@users.noreply.github.com>
2021-03-05 19:02:31 +0100

    Add files via upload

---
+++ wordcounter2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace wordcounter2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string str;
+            int i, wrd, l;
+
+            
+            Console.Write("Type a phrase or sentence or paragraph for words to be counted: ");
+            str = Console.ReadLine();
+
+            l = 0;
+            wrd = 1;
+
+            
+            while (l <= str.Length - 1)
+            {
+                
+                if (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
+                {
+                    wrd++;
+                }
+
+                l++;
+            }
+
+            Console.Write("Word count: {0}\n", wrd);
+        }
+    }
+}
